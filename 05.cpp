017df06ef8273f607c35f16a522b9b31f7c57c40
Emil Ahlbäck <emil.ahlback@iteam.se>
2021-06-28 22:10:09 +0200

    Combined

--- 05.cpp
+++ 05.cpp
@@ -8,16 +8,18 @@
 using namespace std;
 
 using Failure = string;
-using Success = pair<char, string>;
+using Success = pair<string, string>;
 
 using Result = variant<Success, Failure>;
+using Parser = function<Result(const string&)>;
 
 auto pchar(char match) {
   return [match](const string& input) -> Result {
     if (input.length() == 0) return Failure { "no more input" };
 
     if (input[0] == match) {
-      return Success { match, input.substr(1) };
+      const string str = string { match };
+      return Success { { match }, input.substr(1) };
     }
 
     return Failure { "Unexpected character" };
@@ -35,13 +37,33 @@ ostream& operator<< (ostream &out, const Result &res) {
   return out;
 }
 
+auto andThen(Parser a, Parser b) {
+  return [a, b](const string& input) -> Result {
+    const auto result1 = a(input);
+
+    if (holds_alternative<Failure>(result1)) return result1;
+    const auto& val_1 = get<Success>(result1);
+    const auto result2 = b(get<1>(val_1));
+
+    if (holds_alternative<Failure>(result2)) return result2;
+    const auto& val_2 = get<Success>(result2);
+
+    const string str1 = get<0>(val_1);
+    const string str2 = get<0>(val_2);
+
+    const string both = str1 + str2;
+    return Success { both, get<1>(val_2) };
+  };
+}
+
 int main() {
   auto parseA = pchar('A');
-  const Result first = parseA("ABC");
-  cout << "first: " << first << "\n";
+  auto parseB = pchar('B');
 
-  const Result second = parseA("ZBC");
-  cout << "second: " << second << "\n";
+  const auto parseAThenB = andThen(parseA, parseB);
+  std::cout << "\"ABC\" " << parseAThenB("ABC") << "\n";
+  std::cout << "\"ZBC\" " << parseAThenB("ZBC") << "\n";
+  std::cout << "\"AZC\" " << parseAThenB("AZC") << "\n";
 
   return 0;
 }
